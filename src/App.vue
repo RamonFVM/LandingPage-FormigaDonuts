19d2ecdaf915be6a6896384ba77bdd393a1a9be2
Ramofvm <ramonfhelipe547@gmail.com>
2025-01-21 01:01:20 -0300

    começo do body

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <MyHeader />
+    <MyBody/>
   </div>
 </template>
 
 <script>
 
 import MyHeader from './components/MyHeader.vue'
+import MyBody from  './components/MyBody.vue'
 
 export default {
   name: 'App',
   components: {
-  
+    MyBody,
     MyHeader
   }
 }
--- src/components/MyBody.vue
+++ src/components/MyBody.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        <img src="" alt="">
+    <div class="bg-[#ffffd0] mb-6">
+      <h1 class="flex justify-center items-center text-4xl font-mono p-4 text-center">
+        Um Pouco sobre nós
+      </h1>
     </div>
-</template>
+    
+    <div class="flex justify-center items-center">
+      <p class="font-semibold text-3xl text-center">
+        Olá, Nós somos a <span class="hover:text-fuchsia-300">Formiga </span> 
+        <span class="hover:text-fuchsia-300">Donuts</span>
+        <br> uma micro empresa voltada para 
+        <span class="hover:text-fuchsia-300">confecção</span>
+        <br> e venda de mini cake 
+        <span class="hover:text-fuchsia-300">donuts</span>.
+        <br> Conheça um pouco mais das nossas 
+        <span class="hover:text-fuchsia-300">delícias</span>
+      </p>
+    </div>
+
+     
+  </template>
+  
