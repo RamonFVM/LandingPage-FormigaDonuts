38cf43b1b8d9d1666789f19f16395857a58598a1
Ramofvm <ramonfhelipe547@gmail.com>
2025-01-26 11:37:02 -0300

    adicionando responsividade

--- src/components/IconsSocial.vue
+++ src/components/IconsSocial.vue
@@ -1,43 +1,38 @@
 <template>
     <div>
-      
+     
       <button @click="toggleSocial" class="fixed-button">
         Clique aqui para visitar
       </button>
   
-      
+   
       <div v-if="Social" class="overlay">
         <div class="card">
-        
+         
           <a href="https://www.instagram.com/formiga_donuts/" target="_blank" class="fixed-icon instagram">
             <span class="material-icons">instagram</span>
           </a>
   
-          <a href="https://wa.me/1234567890" target="_blank" class="fixed-icon whatsapp">
+          <a href="https://wa.me/558399853196" target="_blank" class="fixed-icon whatsapp">
             <span class="material-icons">whatsapp</span>
           </a>
   
-      
-          <button @click="toggleSocial" class="close-btn">Fechar</button>
+         
+          <button @click="toggleSocial" class="close-btn">X</button>
         </div>
       </div>
     </div>
   </template>
   
   <script>
-  
-  
   export default {
-    components: {
-     
-    },
     data() {
       return {
-        Social: false,  
+        Social: false, 
       };
     },
     methods: {
-   
+    
       toggleSocial() {
         this.Social = !this.Social;
       }
@@ -50,10 +45,9 @@
   .fixed-button {
     position: fixed;
     bottom: 20px;
-    width: 15%;
     right: 20px;
     background-color: #ff4081;
-    padding: 10px 20px;
+    padding: 20px 40px;
     border-radius: 5px;
     color: white;
     font-size: 16px;
@@ -67,31 +61,32 @@
     background-color: #f50057;
   }
   
-
+ 
   .overlay {
     position: fixed;
     top: 0;
     left: 0;
     width: 100%;
     height: 100%;
-    background-color: rgba(0, 0, 0, 0.6); 
-    backdrop-filter: blur(8px); 
+    background-color: rgba(0, 0, 0, 0.6);
+    backdrop-filter: blur(8px);
     display: flex;
     justify-content: center;
     align-items: center;
-    z-index: 999; 
+    z-index: 999;
   }
   
 
   .card {
     background-color: white;
-    padding: 20px;
+    padding: 30px;
     border-radius: 10px;
     box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
-    width: 80%;
+    width: 60%;
     max-width: 500px;
     text-align: center;
     position: relative;
+    overflow: hidden;
   }
   
   
@@ -99,7 +94,7 @@
     position: relative;
     bottom: 20px;
     background-color: #ff4081;
-    padding: 15px;
+    padding: 20px;
     border-radius: 16px;
     color: white;
     font-size: 24px;
@@ -125,7 +120,7 @@
     transform: scale(1.1);
   }
   
- 
+
   .close-btn {
     position: absolute;
     top: 10px;
@@ -133,12 +128,62 @@
     background-color: #ff4081;
     color: white;
     border: none;
-    padding: 5px 10px;
-    border-radius: 30%;
+    padding: 10px 20px;
+    border-radius: 50%;
     cursor: pointer;
   }
   
   .close-btn:hover {
     background-color: #f50057;
+  }
+  
+ 
+  @media (max-width: 768px) {
+    .fixed-button {
+      width: 60%;
+      right: 20px;
+      bottom: 15px;
+      font-size: 14px;
+      padding: 16px 32px;
+    }
+  
+    .card {
+      width: 80%;
+      padding: 20px;
+    }
+  
+    .fixed-icon {
+      padding: 15px;
+      font-size: 22px;
+    }
+  
+    .close-btn {
+      padding: 8px 16px;
+    }
+  }
+  
+
+  @media (max-width: 480px) {
+    .fixed-button {
+      width: 70%;
+      right: 10px;
+      bottom: 10px;
+      font-size: 14px;
+      padding: 12px 24px;
+    }
+  
+    .card {
+      width: 50%;
+      padding: 15px;
+    }
+  
+    .fixed-icon {
+      padding: 12px;
+      font-size: 20px;
+    }
+  
+    .close-btn {
+      padding: 6px 12px;
+    }
   }
   </style>
